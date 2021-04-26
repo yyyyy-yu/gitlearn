d3eff49a9964fa9d0e638591744edb84687460b2
pengyu <pengyu@pengyu03.local>
2021-04-26 19:22:37 +0800

    First Commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+  Button button;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    button = findViewById(R.id.button_1);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this,M2Activity.class);
+        startActivity(intent);
+      }
+    });
+  }
+}
